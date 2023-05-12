17000b148d410f6c62e665a1594719f3d7edf758
Gyno Pomare <pomare-08@sandiego.edu>
2023-05-12 08:26:16 -0700

    all done

--- app.js
+++ app.js
@@ -1,7 +1,76 @@
 const express = require("express");
+const postBank = require('./postBank')
+const morgan = require('morgan')
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'))
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+  const post = postBank.list()
+  
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+  <div class="news-list">
+  <header><img src="/logo.png"/>Wizard News</header>
+  ${post.map(post => `
+  <div class='news-item'>
+  <p>
+  <span class="news-position">${post.id}. ▲</span>
+  ${post.title}
+  <small>(by ${post.name})</small>
+  </p>
+  <small class="news-info">
+  ${post.upvotes} upvotes | ${post.date}
+  </small>
+  <a href="/posts/${post.id}">${post.title}</a>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+
+  res.send(html)
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  
+  res.send( `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 const PORT = 1337;
 
